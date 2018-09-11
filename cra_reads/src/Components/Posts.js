3436adc9bc9741c5c7da596ea9d4b7aaef52d26c
luke0b1000 <luke0b1000@gmail.com>
2018-09-10 23:02:34 -0400

    Fix page with no post, 404 page

--- cra_reads/src/Components/Posts.js
+++ cra_reads/src/Components/Posts.js
@@ -4,6 +4,7 @@ import { getCommentsOfPost } from "../utils/API";
 import Comment from "./Comment";
 import AddComments from "./AddComments";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Posts extends Component {
     state = {
@@ -18,26 +19,40 @@ class Posts extends Component {
         this.getComments();
     }
     render() {
-        const  comments  = this.props.CommentsByPostID;
-        const PostDisplay = <Post postID={this.props.match.params.urlPostID} />;
-        const commentList = comments
-            ? comments.map(comment => (
-                  <Comment key={comment.id} commentID={comment.id} />
-              ))
-            : "No Comment";
-        return (
-            <div>
-                {PostDisplay}
-                <p>COMMENTS - THERE ARE {`${comments.length}`} COMMENTS</p>
-                {comments && commentList}
-                <AddComments />
-            </div>
-        );
+        if (this.props.posts) {
+            const comments = this.props.CommentsByPostID;
+            const PostDisplay = (
+                <Post postID={this.props.match.params.urlPostID} />
+            );
+            const commentList = comments
+                ? comments.map(comment => (
+                      <Comment key={comment.id} commentID={comment.id} />
+                  ))
+                : "No Comment";
+            return (
+                <div>
+                    {PostDisplay}
+                    <p>COMMENTS - THERE ARE {`${comments.length}`} COMMENTS</p>
+                    {comments && commentList}
+                    <AddComments />
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <p>404 Error - This page no longer exist.</p>
+                    <p>
+                        <Link to="/">Go back home</Link>
+                    </p>
+                </div>
+            );
+        }
     }
 }
 
-function mapStateToProps({ Comments }, { match }) {
+function mapStateToProps({ Posts, Comments }, { match }) {
     const urlPostID = match.params.urlPostID;
+    const posts = Posts[urlPostID];
     const CommentsKeyArray = Object.keys(Comments);
     const CommentsByPostIDArray = CommentsKeyArray.filter(
         key =>
@@ -45,6 +60,7 @@ function mapStateToProps({ Comments }, { match }) {
     );
     const CommentsByPostID = CommentsByPostIDArray.map(key => Comments[key]);
     return {
+        posts,
         CommentsByPostID
     };
 }
